f580d645c3e30db160e4b1aa84fda822a7a5b6b3
Anuja Ware <anuja@joshsoftware.com>
2016-09-18 20:58:45 +0530

    Minitest setup

--- app/models/chat_bot/category.rb
+++ app/models/chat_bot/category.rb
@@ -2,6 +2,13 @@ module ChatBot
   class Category
     include Mongoid::Document
     field :name, type: String
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: {case_sensitive: false}
+
+    before_validation :strip_name
+
+    def strip_name
+      self.name# = name.truncate
+    end
+
   end
 end
--- test/models/chat_bot/category_test.rb
+++ test/models/chat_bot/category_test.rb
@@ -1,9 +1,23 @@
-require 'test_helper'
+require './test/test_helper'
 
 module ChatBot
   class CategoryTest < ActiveSupport::TestCase
-    test 'should not save without name' do
-      category = Category.new
+    def setup
+      @category = Category.new
+    end
+
+    def test_name
+
+    end
+
+    def test_save_fail_without_name
+      assert_not @category.save
+    end
+    
+    def test_save_fail_for_duplicate_name
+      @category.name = 'Cat1'
+      assert_not @category.save
+      category = categories(:two)
       assert_not category.save
     end
   end
