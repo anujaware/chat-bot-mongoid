d68257eab79efa37180530acbdd9c0a7b5350847
Anuja Ware <anuja@joshsoftware.com>
2016-09-25 21:14:58 +0530

    Commented 'interval' related code as its not in use right now

--- app/models/chat_bot/option.rb
+++ app/models/chat_bot/option.rb
@@ -2,8 +2,11 @@ module ChatBot
   class Option
     include Mongoid::Document
     field :name, type: String
-    field :interval, type: String
-    field :deprecated, type: Mongoid::Boolean
+
+    #Will come in the picture along with the scheduling logic
+    #field :interval, type: String
+
+    field :deprecated, type: Mongoid::Boolean, default: false
 
     belongs_to :dialog, class_name: 'ChatBot::Dialog'
     belongs_to :decision, class_name: 'ChatBot::Dialog', inverse_of: nil, primary_key: :code
@@ -12,6 +15,6 @@ module ChatBot
     validates :dialog, presence: true
     validates :decision, inclusion: { in: Proc.new{|option|
       option.dialog.sub_category.dialog_ids }}, allow_blank: true
-    validates :interval, format: { with: /\ADAY:(\d+)\z/i }, allow_blank: true
+    #validates :interval, format: { with: /\ADAY:(\d+)\z/i }, allow_blank: true
   end
 end
