0a4b07dfe65f9608a9a8cb142a48227e33d37c03
Anuja Ware <anuja@joshsoftware.com>
2016-10-17 11:24:41 +0530

    Dialog import test implementation

--- test/models/chat_bot/dialog_import_test.rb
+++ test/models/chat_bot/dialog_import_test.rb
@@ -54,63 +54,170 @@ module ChatBot
         assert_equal dialog.sub_category, sub_category
       end
 
-=begin
-     7. Create few dialogs which covers all possibilities
-       i. code
-       ii. message
-       iii. user input type = ch/cnt/slt/mlt/ddw/date/attach
-           should throw error if exits wrong format
-       iv. messsage type = txt/utube/vimeo/link/img
-       v. options
-       vi. sub category
-=end
-      ## CREATE A SET OF DIALOGS IN EXCEL SHEET and Match each and everything
       context 'user_input_type of' do
-        it 'T1 should be "ch"'
-        it 'T2 should be "cnt"'
-        it 'T3 should be "slt"'
-        it 'T4 should be "mlt"'
-        it 'T5 should be "ddw"'
-        it 'T6 should be "date"'
-        it 'T7 should be "attach"'
+        it 'T1 should be "ch"' do
+          dilog = Dialog.find_by(code: 'T1')
+          assert dilog.user_input_type, 'ch'
+        end
+
+        it 'T2 should be "cnt"' do
+          dilog = Dialog.find_by(code: 'T2')
+          assert dilog.user_input_type, 'cnt'
+        end
+
+        it 'T3 should be "slt"' do
+          dilog = Dialog.find_by(code: 'T3')
+          assert dilog.user_input_type, 'slt'
+        end
+
+        it 'T4 should be "mlt"' do
+          dilog = Dialog.find_by(code: 'T4')
+          assert dilog.user_input_type, 'mlt'
+        end
+
+        it 'T5 should be "ddw"' do
+          dilog = Dialog.find_by(code: 'T5')
+          assert dilog.user_input_type, 'ddw'
+        end
+
+        it 'T6 should be "date"' do
+          dilog = Dialog.find_by(code: 'T6')
+          assert dilog.user_input_type, 'date'
+        end
+
+        it 'T7 should be "attach"' do
+          dilog = Dialog.find_by(code: 'T7')
+          assert dilog.user_input_type, 'attach'
+        end
       end
 
       context 'message_type of' do
-        it 'T2 should be "txt"'
-        it 'T3 should be "utube"'
-        it 'T4 should be "vimeo"'
-        it 'T5 should be "link"'
-        it 'T6 should be "img"'
+        it 'T2 should be "txt"' do
+          dilog = Dialog.find_by(code: 'T1')
+          assert dilog.message_type, 'txt'
+        end
+
+        it 'T3 should be "utube"' do
+          dilog = Dialog.find_by(code: 'T3')
+          assert dilog.user_input_type, 'utube'
+        end
+
+        it 'T4 should be "vimeo"' do
+          dilog = Dialog.find_by(code: 'T4')
+          assert dilog.user_input_type, 'vimeo'
+        end
+
+        it 'T5 should be "link"' do
+          dilog = Dialog.find_by(code: 'T5')
+          assert dilog.user_input_type, 'link'
+        end
+
+        it 'T6 should be "img"' do
+          dilog = Dialog.find_by(code: 'T6')
+          assert dilog.user_input_type, 'img'
+        end
       end
 
       context 'dialog T1 should have 3 options having' do
         context 'one option with' do
-          it 'name "Ok"'
-          it 'interval "DAY:3"'
-          it 'decision T2'
+          it 'name "Ok"' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'Ok')
+            assert option.present?
+          end
+
+          it 'interval "DAY:3"' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'Ok')
+            assert_equal option.interval, 'DAY:3'
+          end
+
+          it 'decision T2' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'Ok')
+            assert_equal option.decision, Dialog.find_by(code: 'T2')
+          end
         end
+
         context 'second option with' do
-          it 'name "Yes"'
-          it 'interval "DAY:1"'
-          it 'decision T2'
+          it 'name "Yes"' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'Yes')
+            assert option.present?
+          end
+
+          it 'interval "DAY:1"' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'Yes')
+            assert_equal option.interval, 'DAY:3'
+          end 
+
+          it 'decision T2' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'Yes')
+            assert_equal option.decision, Dialog.find_by(code: 'T2')
+          end
         end
         context 'third option with' do
-          it 'name "No"'
-          it "interval ''(empty/nil)"
-          it 'decision T3'
+          it 'name "No"' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'No')
+            assert option.present?
+          end
+
+          it "interval ''(empty/nil)" do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'No')
+            assert !option.interval.present?
+          end
+
+          it 'decision T3' do
+            dilog = Dialog.find_by(code: 'T1')
+            option = dilog.options.find_by(name: 'No')
+            assert_equal option.decision, Dialog.find_by(code: 'T3')
+          end
         end
       end
 
       context 'dialog T2 should have 2 options having' do
         context 'one option with' do
-          it 'name "Yes"'
-          it 'interval "DAY:1"'
-          it 'decision T2'
+          it 'name "Yes"' do
+            dilog = Dialog.find_by(code: 'T2')
+            option = dilog.options.find_by(name: 'Yes')
+            assert option.present?
+          end
+
+          it 'interval "DAY:2"' do
+            dilog = Dialog.find_by(code: 'T2')
+            option = dilog.options.find_by(name: 'Yes')
+            assert_equal option.interval, 'DAY:2'
+          end
+
+          it 'decision T2' do
+            dilog = Dialog.find_by(code: 'T2')
+            option = dilog.options.find_by(name: 'Yes')
+            assert !option.decision.present?
+          end
         end
+
         context 'second option with' do
-          it 'name "No"'
-          it "interval ''(empty/nil)"
-          it 'decision T3'
+          it 'name "No"' do
+            dilog = Dialog.find_by(code: 'T2')
+            option = dilog.options.find_by(name: 'No')
+            assert option.present?
+          end
+
+          it "interval ''(empty/nil)" do
+            dilog = Dialog.find_by(code: 'T2')
+            option = dilog.options.find_by(name: 'No')
+            assert !option.interval.present?
+          end
+
+          it 'decision T3' do
+            dilog = Dialog.find_by(code: 'T2')
+            option = dilog.options.find_by(name: 'No')
+            assert_equal option.decision, Dialog.find_by(code: 'T3')
+          end
         end
       end
     end
