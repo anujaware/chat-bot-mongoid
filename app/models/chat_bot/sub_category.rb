75e8efce63b8391996c32ffe419568b8a3e6f78d
Anuja Ware <anuja@joshsoftware.com>
2016-12-12 00:14:06 +0530

    Subcategory model updated as per Rubocop

--- app/models/chat_bot/sub_category.rb
+++ app/models/chat_bot/sub_category.rb
@@ -1,17 +1,18 @@
 module ChatBot
+  ## SubCategory
   class SubCategory
     include Mongoid::Document
     include Mongoid::Timestamps
     include Mongoid::Slug
 
     include Mongoid::History::Trackable
-    track_history :on => [:fields],
-                  :modifier_field => :modifier
+    track_history on: [:fields],
+                  modifier_field: :modifier
 
-    AFTER_DIALOG = 'after_dialog'
-    AFTER_DAYS = 'after_days'
-    IMMEDIATE = 'immediate'
-    STARTS_ON = [AFTER_DIALOG, AFTER_DAYS, IMMEDIATE]
+    AFTER_DIALOG = 'after_dialog'.freeze
+    AFTER_DAYS = 'after_days'.freeze
+    IMMEDIATE = 'immediate'.freeze
+    STARTS_ON = [AFTER_DIALOG, AFTER_DAYS, IMMEDIATE].freeze
 
     field :name, type: String
     field :description, type: String
@@ -22,21 +23,24 @@ module ChatBot
     field :is_ready_to_schedule, type: Boolean, default: false
 
     belongs_to :category, class_name: 'ChatBot::Category'
-    belongs_to :initial_dialog, class_name: 'ChatBot::Dialog', inverse_of: nil, primary_key: :code
-    has_many :dialogs, class_name: 'ChatBot::Dialog'#, foreign_key: :code
+    belongs_to :initial_dialog, class_name: 'ChatBot::Dialog',
+                                inverse_of: nil, primary_key: :code
+    has_many :dialogs, class_name: 'ChatBot::Dialog' # , foreign_key: :code
 
     slug :name
 
-    scope :ready, -> {where(is_ready_to_schedule: true)}
+    scope :ready, -> { where(is_ready_to_schedule: true) }
 
-    index({_slug: 1})
+    index(_slug: 1)
 
-    validates :name, presence: true, uniqueness: { case_sensitive: false, scope: [:category] }
-    validates :category, presence: true #initial_dialog
-    validates :priority, numericality: {only_integer: true, greater_than: 0, less_than: 11}
+    validates :name, presence: true, uniqueness: { case_sensitive: false,
+                                                   scope: [:category] }
+    validates :category, presence: true # initial_dialog
+    validates :priority, numericality: { only_integer: true,
+                                         greater_than: 0, less_than: 11 }
     validates :starts_on_key, inclusion: { in: STARTS_ON }
 
-    before_validation :squish_name, if: "name.present?"
+    before_validation :squish_name, if: 'name.present?'
 
     ## Class methods
     def self.start(sub_category)
@@ -46,24 +50,29 @@ module ChatBot
 
     def self.next_dialog(option_id)
       option = Option.find_by(id: option_id)
-      return nil if !option or !option.decision
+      return nil if !option || !option.decision
       dialog = option.decision
       dialog.data_attributes
     end
 
     ## Object methods
     def squish_name
-      # Squish doesn't work if name contains new line character in single quote while testing
+      # Squish doesn't work if name contains new line character
+      #   in single quote while testing
       # TODO: Fix using gsub if issue occur in application
       self.name = name.squish.capitalize
     end
 
     def self.find_or_create(category, sub_category_name)
-      cat_exist = category.sub_categories.detect{|sub_category|
-        sub_category.name.downcase.strip.gsub(' ', '') == sub_category_name.downcase.strip.gsub(' ', '')
-      }
-      sub_category = cat_exist.present? ? cat_exist : create(name: sub_category_name.strip, category: category)
+      cat_exist = category.sub_categories.detect do |sub_category|
+        compact_name(sub_category.name) == compact_name(sub_category_name)
+      end
+      return cat_exist if cat_exist.present?
+      create(name: sub_category_name.strip, category: category)
     end
 
+    def self.compact_name(str)
+      str.downcase.strip.gsub(' ', '')
+    end
   end
 end
